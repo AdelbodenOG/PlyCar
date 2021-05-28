29dd98c8ceeb25d05513c3d2e8b619b6445fde15
AdelbodenOG <adelbodengo@gmail.com>
2021-05-28 09:31:14 +0100

    new gear stength

--- js/control.js
+++ js/control.js
@@ -9,37 +9,27 @@ var motor = new gpio(14, {mode: gpio.OUTPUT})
 var panServo = new gpio(17, {mode: gpio.OUTPUT})
 var tiltServo = new gpio(27, {mode: gpio.OUTPUT})
 
-async function sleep(ms) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve()
-        }, ms);
-    })
-}
+client.on("data", data => {
+    var controls = JSON.parse(data.toString())
 
-async function init(def){
+    var axis = controls.axis * 500 + 1500
+    var speed = (controls.gear * 50) * controls.speed + 1500
+    var pan = controls.pan * 500 + 1500
+    var tilt = controls.tilt * 500 + 1500
+
+    console.log(speed)
+
+    servo.servoWrite(axis)
+    motor.servoWrite(speed)
+    panServo.servoWrite(pan)
+    tiltServo.servoWrite(tilt)
+})
+
+function init(def){
     servo.servoWrite(def)
     motor.servoWrite(def)
     panServo.servoWrite(def)
     tiltServo.servoWrite(def)
-
-    await sleep(1000)
-
-    client.on("data", data => {
-        var controls = JSON.parse(data.toString())
-    
-        var axis = controls.axis * 500 + 1500
-        var speed = (controls.gear * 100) * controls.speed + 1500
-        var pan = controls.pan * 500 + 1500
-        var tilt = controls.tilt * 500 + 1500
-    
-        console.log(speed)
-    
-        servo.servoWrite(axis)
-        motor.servoWrite(speed)
-        panServo.servoWrite(pan)
-        tiltServo.servoWrite(tilt)
-    })
 }
 
 init(1500)
